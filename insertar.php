0b81511313ef04a520665ee601f92d25920b07a2
Aitor Blesa <38209.joan23@fje.edu>
2016-01-27 18:22:04 +0100

    recogida latitud, longitud y direccion a partir de estos

--- insertar.php
+++ insertar.php
@@ -9,12 +9,11 @@ if(empty($_SESSION['login_user'])){
 <!DOCTYPE html>
 <!--transformar en html5  -->
 <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html">
-		<meta charset="utf-8">
-		<meta name="description" content="sesiones">
-		<link rel="stylesheet" type="text/css" href="css/styles.css">
-      <link rel="stylesheet" type="text/css" href="css/insertar.css">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html">
+    <meta charset="utf-8">
+    <meta name="description" content="sesiones">
+
       <script type="text/javascript" src="js/maps.js"></script>
       <!-- api de google maps  -->
       <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBW_5m9Miz3xloHRp1QF3cW5V_I0qoz3RI&callback=initMap" async defer></script>
@@ -23,27 +22,26 @@ if(empty($_SESSION['login_user'])){
             initMap();
          }
       </script>
-		<body>
-		<section class="envoltura">
-		<header>
-			<p><a href="login/logout.php" class="logout" title="Logout"><img src="img/logout_white.png" alt="Logout"></a>
-			<?php echo $_SESSION['nombre_usuario']. " " .$_SESSION['apellido_usuario']?></p>
-			<h1 id="h1Agenda">Agenda de Contactos</h1>
-		</header>
+    <body>
+    <section class="envoltura">
+    <header>
+      <p><a href="login/logout.php" class="logout" title="Logout"><img src="img/logout_white.png" alt="Logout"></a>
+      <?php echo $_SESSION['nombre_usuario']. " " .$_SESSION['apellido_usuario']?></p>
+      <h1 id="h1Agenda">Agenda de Contactos</h1>
+    </header>
       <article>
          <form id="formulario_box" action="insertar.proc.php" method="get">
             <p>Nombre:   <input type="text" name="nombre" required></p>
             <p>Apellidos:<input type="text" name="apellidos" required></p>
             <p>Email:    <input type="email" name="mail" required title="Formato correcto: jose@mycontacts.com"></p>
             <p>Teléfono: <input type="tel"  name="telefono" pattern="[0-9]{9}" required></p>
-            <p>Direción: <input type="text" name="direccion" required></p>
-            <p>Latitud: <input type="text" id="latitud" name="latitud" value="" size="30"></p>
-            <p>Longitud:<input type="text" id="longitud" name="longitud" value="" size="30"></p>
+            <p>Direción: <input type="text" name="direccion" id="direccion" required></p>
+            <input id="coordenadas" type="text" value="">
             <div id="map" style="width:600px;height:500px;"></div><br/>
             <input type="submit" name="enviar" value="Enviar">
          </form>
-	   </article>
+     </article>
 
-		</section>
-	</body>
+    </section>
+  </body>
 </html>
--- js/maps.js
+++ js/maps.js
@@ -1,42 +1,47 @@
-var map, latitud, longitud;
-var geocoder = new google.maps.Geocoder;
-var infowindow = new google.maps.InfoWindow;
-//funcion para iniciar el mapa
-function initMap() {
-  //se asigna la situación elegida (Hospitalet)
-  var centro = new google.maps.LatLng(41.368115, 2.116750);
+  var map, latitud, longitud, direccion;
 
-  // se genera la variable map que contiene el objeto mapa y sus características
-  map = new google.maps.Map(document.getElementById('map',centro), {
+function initMap() {
+  var centro = new google.maps.LatLng(41.368115, 2.116750);
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
     center: centro,
-    zoom: 13
   });
 
-  //se asigna un evento al mapa cuando se le hace click.
-  google.maps.event.addListener(map, 'click', function(event) {
+  var geocoder = new google.maps.Geocoder;
+  var infowindow = new google.maps.InfoWindow;
+
+
+/* document.getElementById('submit').addEventListener('click', function() {
+    geocodeLatLng(geocoder, map, infowindow);
+  });*/
+google.maps.event.addListener(map, 'click', function(event) {
     //se asigna a los input latitud y longitud las coordenadas del click
     latitud = event.latLng.lat();
     longitud = event.latLng.lng()
-    document.getElementById('latitud').value=latitud+", "+longitud;
+    document.getElementById('coordenadas').value=latitud+", "+longitud;
     //document.getElementById('latitud').value=latitud;
-    //document.getElementById('longitud').value=longitud;
+   // document.getElementById('longitud').value=longitud;
     geocodeLatLng(geocoder, map, infowindow);
+
     });
 }
 
 function geocodeLatLng(geocoder, map, infowindow) {
-  var input = document.getElementById('latitud').value;
+  var input = document.getElementById('coordenadas').value;
   var latlngStr = input.split(',', 2);
   var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
   geocoder.geocode({'location': latlng}, function(results, status) {
     if (status === google.maps.GeocoderStatus.OK) {
       if (results[1]) {
-        map.setZoom(11);
         var marker = new google.maps.Marker({
           position: latlng,
           map: map
         });
-        infowindow.setContent(results[1].formatted_address);
+        direccion = results[1].formatted_address;
+        infowindow.setContent(direccion);
+      document.getElementById('direccion').value=direccion;
+       
+
         infowindow.open(map, marker);
       } else {
         window.alert('No results found');
